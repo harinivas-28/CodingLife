b87423da0e9c40d0a7b9f20a67eb68b3c1d4ab57
harinivas-28 <harinivasg28704@gmail.com>
2025-02-08 16:44:50 +0530

    Implement tree construction and traversal methods in Day7P2_Trees.java;

--- Coding/java/Day7P2_Trees.java
+++ Coding/java/Day7P2_Trees.java
@@ -48,19 +48,65 @@ Explanation:
 
  */
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class Day7P2_Trees {
+    private static int preInd = 0;
+    private static List<Integer> res;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] pre = Arrays.stream(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int[] post = Arrays.stream(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        List<Integer> pre = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Integer> post = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
         TreeNode root = buildTree(pre, post);
+        res = new ArrayList<>();
+        inorder(root);
+        System.out.println("In-Order: "+res);
+        List<List<Integer>> lvl = levelOrder(root);
+        System.out.println("Level Order: "+lvl);
+        sc.close();
     }
-    private static TreeNode buildTree(int[] pre, int[] post){
-        return null;
-        // TODO : Implement this function tomorrow
+    private static List<List<Integer>> levelOrder(TreeNode root){
+        if(root==null) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode tempNode = q.poll();
+                if(tempNode!=null){
+                    temp.add(tempNode.val);
+                    if(tempNode.left!=null) q.offer(tempNode.left);
+                    if(tempNode.right!=null) q.offer(tempNode.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+    private static void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        res.add(root.val);
+        inorder(root.right);
+    }
+    private static TreeNode buildTree(List<Integer> pre, List<Integer> post){
+        return buildTreeHelper(pre, post, 0, post.size()-1);
+    }
+    private static TreeNode buildTreeHelper(List<Integer> pre, List<Integer> post, int start, int end){
+        if(preInd >= pre.size() || start> end) return null;
+        int rootVal = pre.get(preInd++); // Todo: Learn how this is working!
+        TreeNode root = new TreeNode(rootVal);
+        if(start==end) return root;
+        int leftChild = pre.get(preInd);
+        int idx = post.indexOf(leftChild);
+        root.left = buildTreeHelper(pre, post, start, idx);
+        root.right = buildTreeHelper(pre, post, idx+1, end-1);
+        return root;
     }
 }
