8af0d4727889f5c45a06f1f7bbef2351862d4c04
harinivas-28 <harinivasg28704@gmail.com>
2025-03-25 01:00:31 +0530

    Update input and output files; refactor solution logic in solution.cpp

--- practise/compet/solution.cpp
+++ practise/compet/solution.cpp
@@ -1,52 +1,42 @@
 #include <bits/stdc++.h> 
 #define ll long long int
 #define nl "\n"
+#define pb push_back
+#define ppb pop_back
 #define Fast()                              \
     ios_base::sync_with_stdio(false);       \
     cin.tie(NULL);                          \
     cout.tie(NULL);
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-
-struct comp {
-    // Operator() overloading
-    bool operator()(const pair<ll, ll>& p1, const pair<ll, ll>& p2) {
-        // new definition
-        return p1.second < p2.second;
-    }
-};
-
-template <typename T> using PQ = priority_queue<T>;
-template <typename T> using QP = priority_queue<T, vector<T>, greater<T>>;
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T, typename R> using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T, typename R> using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-ll BigMod(ll b, ll p, ll m) {
-    if (p == 0) return 1;
-    if (p % 2 == 0) {
-        ll s = BigMod(b, p / 2, m);
-        return ((s % m) * (s % m)) % m;
-    }
-    return ((b % m) * (BigMod(b, p - 1, m) % m)) % m;
-}
-
-ll mod = 1e9 + 7;
-
-void solve() {
     
+using namespace std;
+ll mod = 1e9+7;
+
+void solve(){
+    int n; cin >> n;
+	int v[n];
+	for(int i=1;i<n;i++) cin >> v[i];
+	vector<int> res;
+	res.pb(n);
+	int curr = v[n-1];
+	while(curr!=1){
+		res.pb(curr);
+		curr = v[curr-1];
+	}
+	res.pb(1);
+	for(int i=res.size()-1;i>=0;i--){
+		cout << res[i] << " ";
+	}
+	cout << nl;
 }
 
-int main() {
-    Fast();
+int main()
+{
+	Fast();
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    ll t;
-    cin >> t;
-    while (t--) solve();
+    // ll t;
+    // cin >> t;
+    // while (t--) 
+    solve();
 }
 
