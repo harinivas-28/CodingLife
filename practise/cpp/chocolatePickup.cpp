6011781e63ef424de6ec0ed2035197a819567e71
harinivas-28 <harinivasg28704@gmail.com>
2025-04-14 20:31:16 +0530

    Refactor chocolate pickup solution to initialize the last row of the DP table and optimize the processing of rows for better clarity and performance

--- practise/cpp/chocolatePickup.cpp
+++ practise/cpp/chocolatePickup.cpp
@@ -2,62 +2,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// gfg
+// lc-741
 // } Driver Code Ends
 
 class Solution {
   public:
     int solve(int n, int m, vector<vector<int>>& grid) {
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1e8)));
-        
-        // Initialize first row
-        for(int j1=0; j1<m; j1++) {
-            for(int j2=0; j2<m; j2++) {
-                if(j1 == j2)
-                    dp[0][j1][j2] = grid[0][j1];
-                else
-                    dp[0][j1][j2] = grid[0][j1] + grid[0][j2];
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m , 0)));
+        // Initialize last row
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]; 
             }
         }
         
-        // Fill dp table
-        for(int i=1; i<n; i++) {
-            for(int j1=0; j1<m; j1++) {
-                for(int j2=0; j2<m; j2++) {
+        // Process rest of the rows
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
                     int maxi = -1e8;
-                    // Try all possible movements
-                    for(int dj1=-1; dj1<=1; dj1++) {
-                        for(int dj2=-1; dj2<=1; dj2++) {
-                            int value = -1e8;
-                            int nj1 = j1 + dj1;
-                            int nj2 = j2 + dj2;
+                    for(int dj1=-1;dj1<=+1;dj1++){
+                        for(int dj2=-1;dj2<=+1;dj2++){
+                            int val = 0;
+                            if(j1==j2) 
+                                val = grid[i][j1];
+                            else 
+                                val = grid[i][j1] + grid[i][j2];
+                                
+                            int newJ1 = j1 + dj1;
+                            int newJ2 = j2 + dj2;
                             
-                            if(nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m) {
-                                value = dp[i-1][nj1][nj2];
-                                if(j1 == j2)
-                                    value += grid[i][j1];
-                                else
-                                    value += grid[i][j1] + grid[i][j2];
-                            }
-                            maxi = max(maxi, value);
+                            if(newJ1>=0 && newJ2>=0 && newJ1<m && newJ2<m)
+                                val += dp[i+1][newJ1][newJ2];
+                            else 
+                                val = -1e8;
+                                
+                            maxi = max(maxi, val);
                         }
                     }
                     dp[i][j1][j2] = maxi;
                 }
             }
         }
-        
-        // Find maximum value in last row
-        int result = -1e8;
-        for(int j1=0; j1<m; j1++) {
-            for(int j2=0; j2<m; j2++) {
-                result = max(result, dp[n-1][j1][j2]);
-            }
-        }
-        return result;
+        return dp[0][0][m-1];
     }
 };
 
+/*
+class Solution {
+    public:
+        int f(int i, int j1, int j2, int n, int m, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+            if(j1<0 || j2<0 || j1>=m || j2>=m) return -1e8;
+            if(i==n-1){
+                if(j1==j2) return grid[i][j1];
+                return grid[i][j1] + grid[i][j2];
+            }
+            if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+            int maxi = -1e8;
+            for(int dj1=-1;dj1<=+1;dj1++){
+                for(int dj2=-1;dj2<=+1;dj2++){
+                    int val = 0;
+                    if(j1==j2) val = grid[i][j1];
+                    else val = grid[i][j1] + grid[i][j2];
+                    val += f(i+1, j1+dj1, j2+dj2, n, m, grid, dp);
+                    maxi = max(maxi, val);
+                }
+            }
+            return dp[i][j1][j2] = maxi;
+        }
+        int solve(int n, int m, vector<vector<int>>& grid) {
+            // code here
+            vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+            return f(0, 0, m-1, n, m, grid, dp);
+        }
+    };
+*/
 
 //{ Driver Code Starts.
 
