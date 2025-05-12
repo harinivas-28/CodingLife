65edeff539eee3c0e776ed515dbc65cb65de9ebf
harinivas-28 <harinivasg28704@gmail.com>
2025-05-12 12:09:14 +0530

    Add implementations for M-Coloring Problem and maximum weighted path in DAG

--- practise/java/graphColoring.java
+++ practise/java/graphColoring.java
@@ -1,4 +1,79 @@
 package practise.java;
+import java.util.*;
+/*
+M-Coloring Problem
+Difficulty: MediumAccuracy: 34.42%Submissions: 153K+Points: 4Average Time: 45m
+You are given an undirected graph consisting of V vertices and E edges represented by a list edges[][], along with an integer m. Your task is to determine whether it is possible to color the graph using at most m different colors such that no two adjacent vertices share the same color. Return true if the graph can be colored with at most m colors, otherwise return false.
 
+Note: The graph is indexed with 0-based indexing.
+
+Examples:
+
+Input: V = 4, edges[][] = [[0, 1], [1, 3], [2, 3], [3, 0], [0, 2]], m = 3
+Output: true
+Explanation: It is possible to color the given graph using 3 colors, for example, one of the possible ways vertices can be colored as follows:
+
+Vertex 0: Color 1
+Vertex 1: Color 2
+Vertex 2: Color 2
+Vertex 3: Color 3
+Input: V = 3, edges[][] = [[0, 1], [1, 2], [0, 2]], m = 2
+Output: false
+Explanation: It is not possible to color the given graph using only 2 colors because vertices 0, 1, and 2 form a triangle.
+Constraints:
+1 ≤ V ≤ 10
+1 ≤ E = edges.size() ≤ (V*(V-1))/2
+0 ≤ edges[i][j] ≤ V-1
+1 ≤ m ≤ V
+
+Expected Complexities
+Time Complexity: O(V * m ^ V)
+Auxiliary Space: O(V + E)
+Company Tags
+Amazon
+ */
 public class graphColoring {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int V = sc.nextInt();
+        int r = sc.nextInt();
+        List<int[]> edges = new ArrayList<>();
+        for(int i=0;i<r;i++){
+            int u = sc.nextInt(), v = sc.nextInt();
+            edges.add(new int[]{u, v});
+        }
+        int m = sc.nextInt(); // m colors
+        System.out.println(colorGraph(V, edges, m));
+    }
+    private static boolean colorGraph(int V, List<int[]> edges, int m){
+        List<List<Integer>> adj = new ArrayList<>();
+        Collections.fill(adj, new ArrayList<>());
+        for(int[] e: edges){
+            int u = e[0], v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int[] col = new int[V];
+        Arrays.fill(col, -1);
+        return dfs(0, adj, col, m);
+    }
+    private static boolean dfs(int idx, List<List<Integer>> adj, int[] col, int m){
+        if(idx==col.length) return true;
+        for(int i=0;i<m;i++){
+            if(valid(i, idx, adj, col)){
+                col[idx] = i;
+                if(dfs(idx+1, adj, col, m)){
+                    return true;
+                }
+                col[idx] = -1;
+            }
+        }
+        return false;
+    }
+    private static boolean valid(int i, int idx, List<List<Integer>> adj, int[] col){
+        for(int x: adj.get(idx)){
+            if(col[x]!=-1 && col[x]==i) return false;
+        }
+        return true;
+    }
 }
--- practise/java/maxWeightedGraph.java
+++ practise/java/maxWeightedGraph.java
@@ -1,4 +1,64 @@
 package practise.java;
+import java.util.*;
+/*
+You are given an integer n and a Directed Acyclic Graph (DAG) with n nodes labeled from 0 to n - 1. This is represented by a 2D array edges, where edges[i] = [ui, vi, wi] indicates a directed edge from node ui to vi with weight wi.
 
+You are also given two integers, k and t.
+
+Your task is to determine the maximum possible sum of edge weights for any path in the graph such that:
+
+The path contains exactly k edges.
+The total sum of edge weights in the path is strictly less than t.
+Return the maximum possible sum of weights for such a path. If no such path exists, return -1.
+ */
+/*HINT : the problem is asking for a path with exactly k edges in a DAG, not just any combination of k edges, the previous approach is invalid. We can't freely choose any k edges; the selected edges must form a valid path, which is a much stricter requirement.
+ */
 public class maxWeightedGraph {
+    static Integer[][][] memo;
+    static Map<Integer, List<int[]>> graph;
+    static int res = 0;
+    static int t;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        List<int[]> edges = new ArrayList<>();
+        for(int i=0;i<r;i++){
+            int u = sc.nextInt(), v = sc.nextInt(), w=sc.nextInt();
+            edges.add(new int[]{u, v, w});
+        }
+        int k =sc.nextInt();
+        int t = sc.nextInt();
+        memo = new Integer[n][k+1][t];
+        sc.close();
+        System.out.println(maxWeight(n, edges, k, t));
+    }
+    private static int maxWeight(int n, List<int[]> edges, int k, int t){
+        graph = new HashMap<>();
+        for(int[] e: edges){
+            graph.computeIfAbsent(e[0], x->new ArrayList<>()).add(new int[]{e[1], e[2]});
+        }
+        int res = -1;
+        for(int u=0;u<n;u++){
+            int ans = dfs(u, k, 0);
+            res = Math.max(res, ans);
+        }
+        return res==Integer.MIN_VALUE ? -1 : res;
+    }
+    private static int dfs(int u, int k, int sum){
+        if(k==0) return sum<t ? sum : Integer.MIN_VALUE;
+        if(sum>=t) return Integer.MIN_VALUE;
+        if(memo[u][k][sum]!=null) return memo[u][k][sum];
+        int maxSum = Integer.MIN_VALUE;
+        for(int[] e: graph.get(u)){
+            int v = e[0], wt = e[1];
+            if(sum+wt<t){
+                int next = dfs(v, k-1, sum+wt);
+                if(next!=Integer.MIN_VALUE){
+                    maxSum = Math.max(maxSum, next);
+                }
+            }
+        }
+        return memo[u][k][sum] = maxSum;
+    }
 }
