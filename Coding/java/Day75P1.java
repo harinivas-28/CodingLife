c8e76160b23d398077371a0a22814528445d0469
harinivas-28 <harinivasg28704@gmail.com>
2025-07-07 14:35:12 +0530

    Added Day75 Problem and Solution;Addded Deployment of React App in EC2 instance

---
+++ Coding/java/Day75P1.java
@@ -0,0 +1,66 @@
+package Coding.java;
+/*
+Sampoornesh Babu is learning arithmatics.
+His teacher given him a task to find the minimum number of operations
+required to convert a given integer I to 1.
+
+Sampoornesh is allowed to perform the following operations:
+	- If I is even, convert I with I/2 .
+	- In I is odd, convert I with either I+1 or I-1.
+
+Now it's your task to help Sampoornesh Babu to find and print
+the minimum number of operations required.
+
+Input Format:
+-------------
+An integer I.
+
+Output Format:
+--------------
+Print an integer, the minimum number of operations required.
+
+
+Sample Input-1:
+---------------
+10
+
+Sample Output-1:
+----------------
+4
+
+Explanation:
+------------
+10 -> 5 -> 4-> 2 -> 1.
+
+
+Sample Input-2:
+---------------
+15
+
+Sample Output-2:
+----------------
+5
+
+Explanation:
+------------
+15 -> 16 -> 8 -> 4 -> 2 -> 1.
+ */
+import java.util.Scanner;
+public class Day75P1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solve(n));
+    }
+    private static int solve(int n){
+        if(n==1) return 0;
+        if(n<1) return Integer.MAX_VALUE;
+        int steps = Integer.MAX_VALUE;
+        if(n%2==0){
+            steps = Math.min(steps, 1+solve(n/2));
+        } else {
+            steps = 1+ Math.min(solve(n-1), solve(n+1));
+        }
+        return steps;
+    }
+}
