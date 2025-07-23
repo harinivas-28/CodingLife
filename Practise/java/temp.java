9cef99fbf3d806520d400c9f93a95378733f64f4
harinivas-28 <harinivasg28704@gmail.com>
2025-07-23 22:24:16 +0530

    Added Little bit of practice;

--- Practise/java/temp.java
+++ Practise/java/temp.java
@@ -1,5 +1,29 @@
 package Practise.java;
 import java.util.*;
+interface t {
+    void fly();
+    void eat();
+}
+abstract class f implements t {
+    @Override
+    public void eat(){
+        System.out.println("Eating");
+    }
+    abstract void jump();
+    void dash(){
+        System.out.println("Dash");
+    }
+}
+class X extends f {
+    @Override
+    void jump() {
+        System.out.println("Jump");
+    }
+    @Override
+    public void fly() {
+        System.out.println("Fly");
+    }
+}
 public class temp {
     public static void main(String[] args) {
         int[] t = {0, 1,2, 3, 4, 5};
