926ed33567d1dcea07df6b0e84eae5a092c0df2a
Ganjarla Harinivas <150525075+harinivas-28@users.noreply.github.com>
2026-04-12 23:35:08 +0530

    Add #LC1320 Minimum Distance to Type with two fingers
    Add #LC1320 Minimum Distance to Type with two fingers

---
+++ Unsolved/MinDistToType.java
@@ -0,0 +1,60 @@
+/*
+1320. Minimum Distance to Type a Word Using Two Fingers
+Hard
+
+You have a keyboard layout as shown above in the X-Y plane, where each English uppercase letter is located at some coordinate.
+A B C D E F
+G H I J K L
+M N O P Q R
+S T U V W X
+Y Z
+
+For example, the letter 'A' is located at coordinate (0, 0), the letter 'B' is located at coordinate (0, 1), the letter 'P' is located at coordinate (2, 3) and the letter 'Z' is located at coordinate (4, 1).
+Given the string word, return the minimum total distance to type such string using only two fingers.
+
+The distance between coordinates (x1, y1) and (x2, y2) is |x1 - x2| + |y1 - y2|.
+
+Note that the initial positions of your two fingers are considered free so do not count towards your total distance, also your two fingers do not have to start at the first letter or the first two letters.
+
+Example 1:
+Input: word = "CAKE"
+Output: 3
+Explanation: Using two fingers, one optimal way to type "CAKE" is: 
+Finger 1 on letter 'C' -> cost = 0 
+Finger 1 on letter 'A' -> cost = Distance from letter 'C' to letter 'A' = 2 
+Finger 2 on letter 'K' -> cost = 0 
+Finger 2 on letter 'E' -> cost = Distance from letter 'K' to letter 'E' = 1 
+Total distance = 3
+
+Example 2:
+Input: word = "HAPPY"
+Output: 6
+Explanation: Using two fingers, one optimal way to type "HAPPY" is:
+Finger 1 on letter 'H' -> cost = 0
+Finger 1 on letter 'A' -> cost = Distance from letter 'H' to letter 'A' = 2
+Finger 2 on letter 'P' -> cost = 0
+Finger 2 on letter 'P' -> cost = Distance from letter 'P' to letter 'P' = 0
+Finger 1 on letter 'Y' -> cost = Distance from letter 'A' to letter 'Y' = 4
+Total distance = 6
+ 
+Constraints:
+2 <= word.length <= 300
+word consists of uppercase English letters.
+ 
+Acceptance Rate
+70.4%
+Topics
+Principal
+String
+Dynamic Programming
+Weekly Contest 171
+Hint 1
+Use dynamic programming.
+Hint 2
+dp[i][j][k]: smallest movements when you have one finger on i-th char and the other one on j-th char already having written k first characters from word.
+*/
+class Solution {
+    public int minimumDistance(String word) {
+        
+    }
+}
